4fa5aac24301aa0efd9cabc0ee8decdb44cfcc3e
AbdallahALBanna <Abdallah.albanna333@gmail.com>
2024-10-12 22:11:18 +0300

    Add Allure Report

--- src/test/java/SearchProduct/SearchProductTest.java
+++ src/test/java/SearchProduct/SearchProductTest.java
@@ -9,8 +9,7 @@ import static org.testng.Assert.assertEquals;
 public class SearchProductTest extends openBrowser {
 
 
-    @Test
-
+    @Test(priority =1)
     public void testSuccessfulProductSearch() {
 
         SearchResultsPage searchResultsPage = homePage.writeTextInSearchBoxAndClickOk();
@@ -21,6 +20,57 @@ public class SearchProductTest extends openBrowser {
 
         driver.findElement(searchResultsPage.firstSearchResult).click();
 
+    }
+
+    @Test(priority = 2)
+    public void testFailProductSearch() {
+
+        SearchResultsPage searchResultsPage = homePage.writeTextInSearchBoxAndClickOk();
+
+        assertEquals(searchResultsPage.getFirstSearchResultText(),
+                "dyjkdy",
+                "Search test Failed");
+
+        driver.findElement(searchResultsPage.firstSearchResult).click();
+
+    }
+
+    @Test(priority = 2)
+    public void testFailProductSearch1() {
+
+        SearchResultsPage searchResultsPage = homePage.writeTextInSearchBoxAndClickOk();
+
+        assertEquals(searchResultsPage.getFirstSearchResultText(),
+                "dyjkdy",
+                "Search test Failed");
+
+        driver.findElement(searchResultsPage.firstSearchResult).click();
+
+    }
+
+    @Test(priority = 2)
+    public void testFailProductSearch2() {
+
+        SearchResultsPage searchResultsPage = homePage.writeTextInSearchBoxAndClickOk();
+
+        assertEquals(searchResultsPage.getFirstSearchResultText(),
+                "dyjkdy",
+                "Search test Failed");
+
+        driver.findElement(searchResultsPage.firstSearchResult).click();
+
+    }
+
+    @Test(priority = 2)
+    public void testFailProductSearch3() {
+
+        SearchResultsPage searchResultsPage = homePage.writeTextInSearchBoxAndClickOk();
+
+        assertEquals(searchResultsPage.getFirstSearchResultText(),
+                "dyjkdy",
+                "Search test Failed");
+
+        driver.findElement(searchResultsPage.firstSearchResult).click();
 
     }
 
